bea6c35d4426d8ddf0e2c3f8b7977620dc6a8255
CM <grctestemail@gmail.com>
2021-04-24 03:43:02 +0100

    Update NFT.js

--- src/NFT.js
+++ src/NFT.js
@@ -107,10 +107,14 @@ const queryClient = new QueryClient();
 
 function DisplayedNFT (properties) {
   let Apis = properties.apis;
-  let dataProps = properties.data;
 
+  let dataProps = properties.data;
   let id = dataProps.id;
   let issuer = dataProps.issuer;
+  let precision = dataProps.precision;
+  let symbol = dataProps.symbol;
+  let options = dataProps.options;
+  let max_supply = options.max_supply;
 
   const [issuerDetails, setIssuerDetails] = useState();
   const [nftHolder, setNftHolder] = useState();
@@ -142,40 +146,47 @@ function DisplayedNFT (properties) {
   );
 
   let issuerName = issuerDetails ? issuerDetails.name : undefined;
+
+  let permissions = esDetails ? esDetails.permissions : undefined;
+  let asset_flags = esDetails ? esDetails.flags : undefined;
   let dynamic_asset_data = esDetails ? esDetails.dynamic_asset_data : undefined;
   let current_supply = dynamic_asset_data ? dynamic_asset_data.current_supply : undefined;
 
-  let permissions = esDetails ? esDetails.permissions : undefined;
-  let flags = esDetails ? esDetails.flags : undefined;
-
-  let symbol = dataProps.symbol;
-  let precision = dataProps.precision;
-  let options = dataProps.options;
-  let max_supply = options.max_supply;
-
-  let description = JSON.parse(options.description); // json data
+  // Asset Description Langauge
+  let description = JSON.parse(options.description);
   let main = description.main;
   let market = description.market;
   let short_name = description.short_name;
+
+  // Keys expected for type NFT/ART:
+  let nft_object = description.nft_object;
   let nft_signature = description.nft_signature;
 
-  let nft_object = description.nft_object;
-  let artist = nft_object.artist ? nft_object.artist : undefined;
-  let attestation = nft_object.attestation ? nft_object.attestation : undefined;
-  let narrative = nft_object.narrative ? nft_object.narrative : undefined;
-  let title = nft_object.title ? nft_object.title : undefined;
+  // NFT Object
+  // Core keys:
   let type = nft_object.type ? nft_object.type : undefined;
+  let attestation = nft_object.attestation ? nft_object.attestation : undefined;
+  let sig_pubkey_or_address = undefined;
+  if (nft_object.sig_pubkey_or_address) {
+    sig_pubkey_or_address = nft_object.sig_pubkey_or_address;
+  } else  if (nft_object.pubkeyhex) {
+    sig_pubkey_or_address = nft_object.pubkeyhex;
+  }
 
-  /*
-  let tags = nft_object.tags ? nft_object.tags : undefined;
-  let nft_flags = nft_object.flags ? nft_object.flags : undefined;
+  let title = nft_object.title ? nft_object.title : undefined;
+  let artist = nft_object.artist ? nft_object.artist : undefined;
+  let narrative = nft_object.narrative ? nft_object.narrative : undefined;
+  let encoding = nft_object.encoding ? nft_object.encoding : undefined;
+
+  let { image, imgURL } = getImage(nft_object); // retrieving best match media key
+
+  // Optional and Proposed Keys:
+  let tags = nft_object.tags ? nft_object.tags.split(",") : undefined;
+  let nft_flags = nft_object.flags ? nft_object.flags.split(",") : undefined;
+  let acknowledgments = nft_object.acknowledgments ? nft_object.acknowledgments : undefined;
   let license = nft_object.license ? nft_object.license : undefined;
   let holder_license = nft_object.holder_license ? nft_object.holder_license : undefined;
   let password_multihash = nft_object.password_multihash ? nft_object.password_multihash : undefined;
-  */
-
-  let encoding = nft_object.encoding;
-  let { image, imgURL } = getImage(nft_object);
 
   const useStyles = makeStyles((theme) => ({
     paper: {
@@ -197,6 +208,9 @@ function DisplayedNFT (properties) {
     },
     share: {
       margin: theme.spacing(0.25)
+    },
+    button: {
+      margin: theme.spacing(1)
     }
   }));
 
@@ -205,10 +219,10 @@ function DisplayedNFT (properties) {
     setValue(newValue);
   };
 
-  const flagChips = flags
-    ? Object.keys(flags).map(
+  const flagChips = asset_flags
+    ? Object.keys(asset_flags).map(
         (flag) => {
-          const flagValue = flags[flag];
+          const flagValue = asset_flags[flag];
           return flagValue === true
             ? <Chip
                 className={classes.chip}
@@ -220,7 +234,7 @@ function DisplayedNFT (properties) {
       ).filter(x => x)
     : undefined;
 
-  const permissionTips = {
+  const enabledPermissionTips = {
     charge_market_fee : "The asset issuer can enable market fees.",
     white_list : "The asset issuer can create a list of approved markets",
     override_authority : "The asset issuer can transfer this NFT back to themselves.",
@@ -232,27 +246,50 @@ function DisplayedNFT (properties) {
     committee_fed_asset: "This market-issued asset can have its price feeds supplied by Bitshares committee members.",
   };
 
+  const disabledPermissionTips = {
+    charge_market_fee : "No market fees can be charged for this asset; only BTS fees will be required.",
+    white_list : "There is no approved list of markets for this asset. Feel free to trade it for anything on the Bitshares DEX.",
+    override_authority : "The asset issuer is unable to transfer this NFT back to themselves.",
+    transfer_restricted : "This asset's transfers are unrestricted.",
+    disable_force_settle: "This asset cannot be force settled.",
+    global_settle: "This asset cannot be globally settled.",
+    disable_confidential: "Confidential transactions of this asset are permanently possible.",
+    witness_fed_asset: "The witnesses cannot feed a price for this asset.",
+    committee_fed_asset: "The committee cannot feed a price for this asset.",
+  };
+
   const permissionChips = permissions
     ? Object.keys(permissions).map(
         (permission) => {
           const permissionValue = permissions[permission];
-          return permissionValue === true
-            ? <Tooltip
-                TransitionComponent={Zoom}
-                disableFocusListener
-                title={permissionTips[permission]}
-              >
-                <Chip
-                  className={classes.chip}
-                  avatar={<Avatar>{permissionValue === true || permissionValue === 'true'  ? '✔' : '❌'}</Avatar>}
-                  label={permission.replace(/_/g, ' ')}
-                 />
-              </Tooltip>
-            : undefined;
+          return <Tooltip
+                  TransitionComponent={Zoom}
+                  disableFocusListener
+                  title={
+                    permissionValue === true || permissionValue === 'true'
+                      ? enabledPermissionTips[permission]
+                      : disabledPermissionTips[permission]
+                  }
+                >
+                  <Chip
+                    className={classes.chip}
+                    avatar={<Avatar>{permissionValue === true || permissionValue === 'true'  ? '✔' : '❌'}</Avatar>}
+                    label={permission.replace(/_/g, ' ')}
+                   />
+                </Tooltip>;
 
         }
       )
-    : undefined
+    : undefined;
+
+  const tagChips = tags
+    ? tags.map((tag) => {
+      <Chip
+        className={classes.chip}
+        label={tag}
+       />
+      })
+    : undefined;
 
   const shareUrl = `https://btsnft.onrender.com/${symbol}`;
 
@@ -283,11 +320,14 @@ function DisplayedNFT (properties) {
           >
             <Tab label="NFT" {...a11yProps(0)} />
             <Tab label="Asset" {...a11yProps(1)} />
+            <Tab label="Tags" {...a11yProps(6)} />
+            <Tab label="Share" {...a11yProps(8)} />
             <Tab label="Flags" {...a11yProps(2)} />
             <Tab label="Permissions" {...a11yProps(3)} />
             <Tab label="Signature" {...a11yProps(4)} />
-            <Tab label="JSON" {...a11yProps(5)} />
-            <Tab label="Share" {...a11yProps(6)} />
+            <Tab label="License" {...a11yProps(7)} />
+            <Tab label="Where to buy" {...a11yProps(9)} />
+            <Tab label="JSON Data" {...a11yProps(5)} />
           </Tabs>
         </AppBar>
         <TabPanel value={value} index={0}>
@@ -297,8 +337,12 @@ function DisplayedNFT (properties) {
           <Typography variant="body1" gutterBottom>
             <b>Narrative</b>: "{narrative}"
           </Typography>
+          <Typography variant="body1" gutterBottom>
+            <b>Acknowledgments</b>: "{acknowledgments ? acknowledgments : 'N/A'}"
+          </Typography>
         </TabPanel>
         <TabPanel value={value} index={1}>
+          <Chip className={classes.chip} label={`Asset name: ${symbol ? symbol : '???'}`} />
           <Chip className={classes.chip} label={`Current owner: ${nftHolder && nftHolder.length ? nftHolder[0].name : '???'}`} />
           <Chip className={classes.chip} label={`Quantity issued: ${current_supply ? current_supply : '???'}`} />
           <Chip className={classes.chip} label={`File type: ${type ? type : '???'}`} />
@@ -338,36 +382,21 @@ function DisplayedNFT (properties) {
           >
             <Chip className={classes.chip} color={issuerName && issuerName === 'null-account' ? 'primary' : 'secondary'} label={`Asset issuer: ${issuerName}`} />
           </Tooltip>
-          <br/>
-          <br/>
-          <a href={"https://wallet.bitshares.org/#/asset/" + symbol} style={{'padding': '5px'}}>
-            <Button variant="contained">{symbol}</Button>
-          </a>
-          <a href={`https://wallet.bitshares.org/#/market/${symbol}_${market}`}>
-            <Button variant="contained">Make Offer</Button>
-          </a>
         </TabPanel>
         <TabPanel value={value} index={2}>
           {
-            flagChips && flagChips.length
-              ? flagChips
-              : 'No flags are currently enabled.'
+            tagChips && tagChips.length
+              ? tagChips
+              : 'No topic/interest tags.'
           }
+          {
+            nft_flags && nft_flags.length
+              ? nft_flags
+              : 'No NFT tags.'
+          }
+
         </TabPanel>
         <TabPanel value={value} index={3}>
-          {
-            permissionChips && permissionChips.length
-              ? permissionChips
-              : 'All permissions have been disabled.'
-          }
-        </TabPanel>
-        <TabPanel value={value} index={4}>
-          <TextareaAutosize aria-label={"signature"} rowsMin={5} style={{'minWidth': '100%'}} defaultValue={nft_signature ? nft_signature : undefined} />
-        </TabPanel>
-        <TabPanel value={value} index={5}>
-          <TextareaAutosize aria-label={"elasticSearchData"} rowsMin={5} rowsMax={20} style={{'minWidth': '100%'}} defaultValue={esDetails ? JSON.stringify(esDetails) : 'N/A'} />
-        </TabPanel>
-        <TabPanel value={value} index={6}>
 
             <FacebookShareButton
               url={shareUrl}
@@ -501,6 +530,83 @@ function DisplayedNFT (properties) {
             </HatenaShareButton>
 
         </TabPanel>
+        <TabPanel value={value} index={4}>
+          {
+            flagChips && flagChips.length
+              ? flagChips
+              : 'No flags are currently enabled.'
+          }
+        </TabPanel>
+        <TabPanel value={value} index={5}>
+          {
+            permissionChips && permissionChips.length
+              ? permissionChips
+              : 'All permissions have been disabled.'
+          }
+        </TabPanel>
+        <TabPanel value={value} index={6}>
+          <Typography variant="body1" gutterBottom>
+            <b>Signature</b>
+          </Typography>
+          <TextareaAutosize aria-label={"signature"} rowsMin={5} style={{'minWidth': '100%'}} defaultValue={nft_signature ? nft_signature : 'N/A'} />
+          <Typography variant="body1" gutterBottom>
+            <b>Signature pubkey/address</b>
+          </Typography>
+          <TextareaAutosize aria-label={"sig_pubkey_or_address"} rowsMin={5} style={{'minWidth': '100%'}} defaultValue={sig_pubkey_or_address} />
+          <Typography variant="body1" gutterBottom>
+            <b>Password multihash</b>
+          </Typography>
+          <TextareaAutosize aria-label={"password_multihash"} rowsMin={5} style={{'minWidth': '100%'}} defaultValue={password_multihash} />
+        </TabPanel>
+        <TabPanel value={value} index={7}>
+          <Typography variant="body1" gutterBottom>
+            <b>License: </b>
+            {
+              license
+                ? license
+                : 'The license under which this NFT has been released has not been provied.'
+            }
+          </Typography>
+          <Typography variant="body1" gutterBottom>
+            <b>Holder license: </b>
+            {
+              holder_license
+                ? holder_license
+                : 'Holder license information has not been provided.'
+            }
+          </Typography>
+
+        </TabPanel>
+        <TabPanel value={value} index={8}>
+
+          <Typography variant="body1" gutterBottom>
+            The NFT titled "{title}" ({symbol}) can be traded/transfered on the Bitshares DEX
+          </Typography>
+
+          <a href={`https://wallet.bitshares.org/#/market/${symbol}_${market ? market : 'BTS'}`}>
+            <Button className={classes.button} variant="contained">Bitshares.org</Button>
+          </a>
+          <a href={`https://ex.xbts.io/market/${symbol}_${market ? market : 'BTS'}`}>
+            <Button className={classes.button} variant="contained">XBTS.io</Button>
+          </a>
+          <Tooltip
+            TransitionComponent={Zoom}
+            disableFocusListener
+            title={`Within the desktop app search for the UIA "${symbol}"`}
+          >
+            <a href={`https://github.com/bitshares/bitshares-ui/releases`}>
+              <Button className={classes.button} variant="contained">Desktop apps</Button>
+            </a>
+          </Tooltip>
+
+        </TabPanel>
+        <TabPanel value={value} index={9}>
+          <TextareaAutosize aria-label={"elasticSearchData"} rowsMin={5} rowsMax={20} style={{'minWidth': '100%'}} defaultValue={esDetails ? JSON.stringify(esDetails) : 'N/A'} />
+        </TabPanel>
+
+
+
+
       </Paper>
     </Grid>
   );
